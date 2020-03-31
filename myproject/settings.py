cc8b814751719875afd228dab05b28c8c833a1c3
Jiya Uddin Ahmad <jiyauddin4593@gmail.com>
2020-03-31 23:27:42 +0000

    Hello

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&eya(c=jhbngs)8$u7vc$*u(opl=39&u66u(ftp-l16jh&7(_e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jiya4593.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/jiya4593/django_labeltool/static'
