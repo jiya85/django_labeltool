b907e9190ec2e6dd973649f18e9875d67b47047e
Jiya Uddin Ahmad <jiyauddin4593@gmail.com>
2020-04-13 02:28:03 +0600

    Deleted Static_Root

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/jiya4593/django_labeltool/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
